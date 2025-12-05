e9a66991bde5e48d5b214caab2931f458970e7fd
yuguod <60930311+yuguod@users.noreply.github.com>
2025-12-05 09:27:31 +0800

    Update repository URL for cloning store

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -5,7 +5,8 @@ source shell/custom-packages.sh
 #echo "✅ 你选择了第三方软件包：$CUSTOM_PACKAGES"
 # 下载 run 文件仓库
 echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+# git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+git clone --depth=1 https://github.com/yuguod/store.git /tmp/store-run-repo
 
 # 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
 mkdir -p /home/build/immortalwrt/extra-packages
